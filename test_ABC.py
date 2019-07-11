7576916c671a16993993614392b46b700b7a976e
Mark Cooper <cooperm1700@hotmail.co.uk>
2019-07-11 10:48:48 +0100

    Initial tests

--- test_ABC.py
+++ test_ABC.py
@@ -4,5 +4,14 @@ import numpy as np
 def test_logistic():
     print("Hi")
 
-def test_1():
-    assert ABC_investigation.C_function(20, 5, 0, 5, 0) == 5
+def test_t_zero():
+    assert ABC_investigation.C_function(20, 5, 0, 5, np.array([0])) == np.array([5.0])
+
+def test_r_zero():
+    assert ABC_investigation.C_function(20, 0, 0, 5, np.array([5])) == np.array([5.0])
+
+def test_k_zero():
+    assert ABC_investigation.C_function(0, 5, 0, 7, np.array([12])) == np.array([0.0])
+
+def test_large_t():
+    assert np.abs(ABC_investigation.C_function(20, 5, 0, 5, np.array([1000000])) - np.array([20.0])) < 1e-6
