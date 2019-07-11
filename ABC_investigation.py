1e60064fbd39a90cb6540d44e98cc08ccf71fafd
Mark Cooper <cooperm1700@hotmail.co.uk>
2019-07-11 10:48:39 +0100

    Initial tests

--- ABC_investigation.py
+++ ABC_investigation.py
@@ -8,12 +8,14 @@ def C_function(k, r, sigma, C_0, t):
 
     return k / (1 + (-1 + (k / C_0))*exponent) + noise
 
-t_size = np.linspace(0, 4, 100)
 
-Results = C_function(150, 3, 2, 1, t_size)
+if __name__ == "__main__":
+    t_size = np.linspace(0, 4, 100)
 
-print(Results)
+    Results = C_function(150, 3, 2, 1, t_size)
 
-import matplotlib.pyplot as plt
-plt.plot(t_size, Results)
-plt.show()
+    print(Results)
+
+    import matplotlib.pyplot as plt
+    plt.plot(t_size, Results)
+    plt.show()
