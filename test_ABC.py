72defdd40aba3f509b4ea9d218a6ef5242637ab0
Mark Cooper <cooperm1700@hotmail.co.uk>
2019-07-11 12:00:59 +0100

    Initial tests

--- test_ABC.py
+++ test_ABC.py
@@ -14,4 +14,4 @@ def test_k_zero():
     assert ABC_investigation.C_function(0, 5, 0, 7, np.array([12])) == np.array([0.0])
 
 def test_large_t():
-    assert np.abs(ABC_investigation.C_function(20, 5, 0, 5, np.array([1000000])) - np.array([20.0])) < 1e-6
+    assert np.abs(ABC_investigation.C_function(20, 5, 0, 5, np.array([100])) - np.array([20.0])) < 1e-6
